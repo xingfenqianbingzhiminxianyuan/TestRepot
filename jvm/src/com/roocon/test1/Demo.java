762fc8e8648fc20d78b224a73d261199746bb98d
lixianSharp <861034458@qq.com>
2019-04-09 00:37:02 +0800

    Update Demo.java

--- jvm/src/com/roocon/test1/Demo.java
+++ jvm/src/com/roocon/test1/Demo.java
@@ -1,12 +1,12 @@
 package com.roocon.test1;
 
 /**
- * Ìí¼ÓÁË×¢ÊÍ
- * @author ÏÍÔª
+ * æ·»åŠ äº†æ³¨é‡Šå“ˆå“ˆå“ˆ
+ * @author è´¤å…ƒ
  *
  */
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println( "¸Â¸Â ¹ş¹ş¹ş");
+		System.out.println( "å˜å˜ å“ˆå“ˆå“ˆ");
 	}
 }
