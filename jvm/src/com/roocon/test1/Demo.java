c3ac75213cf787274697945ee9e8c1a6219b5455
lixianyuan <861034458@qq.com>
2019-04-09 19:39:02 +0800

    修改了Demo.java文件，修改了注释和打印的内容

--- jvm/src/com/roocon/test1/Demo.java
+++ jvm/src/com/roocon/test1/Demo.java
@@ -1,15 +1,11 @@
 package com.roocon.test1;
 
 /**
- * 添加了注asdfsasdfsdfsdffd释asdfsaasdfsdfdf；asdfsadf斯蒂芬第三方上jasdkfasdfasdf
- * 添加了注asdsdfzdgfsdsdfffsafsdfsafasdfd释asdfsaasdfsdfdf；jasdkfasdfasdf
- *
- * 娣诲姞浜嗘敞閲婂搱鍝堝搱
- * @author 璐ゅ厓
+ *	重新修改了注释
  *
  */
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println( "鍢庡槑 鍝堝搱鍝�");
+		System.out.println( "打印的内容");
 	}
 }
