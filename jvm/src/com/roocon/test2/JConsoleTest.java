415daa1494c150baee4b79bd331c5db332338284
lixianyuan <861034458@qq.com>
2019-04-09 22:18:23 +0800

    提交修改

--- jvm/src/com/roocon/test2/JConsoleTest.java
+++ jvm/src/com/roocon/test2/JConsoleTest.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * VM参数： -Xms100m -Xmx100m -XX:+UseSerialGC 
- * 这段代码的作用是：以128KB/1000毫秒的速度往Java堆中填充数据，一共填充1000次，使用jconsole的“内存”页签进行监视，观察曲线和柱状指示图的变化。
+ * VM鍙傛暟锛� -Xms100m -Xmx100m -XX:+UseSerialGC 
+ * 杩欐浠ｇ爜鐨勪綔鐢ㄦ槸锛氫互128KB/1000姣鐨勯�熷害寰�Java鍫嗕腑濉厖鏁版嵁锛屼竴鍏卞～鍏�1000娆★紝浣跨敤jconsole鐨勨�滃唴瀛樷�濋〉绛捐繘琛岀洃瑙嗭紝瑙傚療鏇茬嚎鍜屾煴鐘舵寚绀哄浘鐨勫彉鍖栥��
  *asdfsadfsadfasdfsadfasdfasdf
  *
  */
@@ -14,7 +14,7 @@ public class JConsoleTest {
 
 	public static void main(String[] args) {
 		System.out.println("helo helel");
-		fill(1000);
+		//在本地仓库中修改
 	}
 
 	private static void fill(int n) {
